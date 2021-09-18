f8f8bf9ae14fb59ac8ce45cc1bbbf4c06e32f1d4
alu <luzf@scc.com.cn>
2021-09-18 13:23:40 +0800

    add

---
+++ micro_serverless_admin/src/main/java/org/example/admin/AdminApplication.java
@@ -0,0 +1,32 @@
+package org.example.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 监控中心
+ * 
+ * @author ruoyi
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AdminApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  监控中心启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
